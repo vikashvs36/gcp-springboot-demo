2b80f339206b10785bc9591e53dddc960e4b04c4
vikash <vikashvs36@gmail.com>
2020-06-04 08:30:43 +0530

    Added Spring boot project

---
+++ src/test/java/com/springboot/gcpdemoproject/GcpDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.gcpdemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpDemoProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
