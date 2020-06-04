488220b8757259b1142856a49f277d410b0b2c92
vikash <vikashvs36@gmail.com>
2020-06-04 08:45:48 +0530

    Add url parameters

--- src/main/java/com/springboot/gcpdemoproject/GcpDemoProjectApplication.java
+++ src/main/java/com/springboot/gcpdemoproject/GcpDemoProjectApplication.java
@@ -2,12 +2,27 @@ package com.springboot.gcpdemoproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GcpDemoProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GcpDemoProjectApplication.class, args);
+	}
+	
+	@GetMapping(path = "/")
+	public String hello() {
+		return "Welcome to GCP World";
+	}
+	
+	@GetMapping(path = "/user/{user}")
+	public String getUser(@PathVariable String user) {
+		user = user!=null ? user : "User";
+		return "Hello "+user;
 	}
 
 }
